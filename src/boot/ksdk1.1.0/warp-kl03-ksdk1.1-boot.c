0631a887af7dbfc2563679ec382e04b07166e7ae
Harry Horsley <hslhcam2@gmail.com>
2020-11-17 18:42:28 +0000

    Added routine for jegger

--- src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-boot.c
+++ src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-boot.c
@@ -2184,6 +2184,24 @@ main(void)
 
                 break;
             }
+                
+            case 'Z':
+            {
+                SEGGER_RTT_WriteString(0, "Starting the Current Sensor Reading Process.");
+                
+                //Configure device
+                numberOfConfigErrors += configureSensorINA219(0x399F,/* Payload: Defaults*/
+                0x0000,/* Calibration standard*/
+                i2cPullupValue
+                );
+                
+                //Read data point from device
+                printSensorDataINA219(0); // No hex mode
+            
+                SEGGER_RTT_WriteString(0, "Fucking completed it mate");
+                
+#endif
+            }
 
             /*
              *    Stress test, including bit-wise toggling and checksum, as well as extensive add and mul, with continous read and write to I2C MMA8451Q
@@ -2555,7 +2573,7 @@ printAllSensors(bool printHeadersAndCalibration, bool hexModeFlag, int menuDelay
                     0x01,/* Normal read 8bit, 800Hz, normal, active mode */
                     i2cPullupValue
                     );
-    #endif
+
     #ifdef WARP_BUILD_ENABLE_DEVMAG3110
     numberOfConfigErrors += configureSensorMAG3110(    0x00,/*    Payload: DR 000, OS 00, 80Hz, ADC 1280, Full 16bit, standby mode to set up register*/
                     0xA0,/*    Payload: AUTO_MRST_EN enable, RAW value without offset */
