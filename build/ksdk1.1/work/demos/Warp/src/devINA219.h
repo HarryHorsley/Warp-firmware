9677abfbccb60f4eda952d154065ba68a32aba9f
Harry Horsley <hslhcam2@gmail.com>
2020-11-18 14:05:33 +0000

    iufjnfdsa

--- build/ksdk1.1/work/demos/Warp/src/devINA219.h
+++ build/ksdk1.1/work/demos/Warp/src/devINA219.h
@@ -13,7 +13,7 @@ void        initINA219(const uint8_t i2cAddress, WarpI2CDeviceState volatile *  
 
 WarpStatus    readSensorRegisterINA219(uint8_t deviceRegister, int numberOfBytes);
 WarpStatus    writeSensorRegisterINA219(uint8_t deviceRegister,
-                    uint8_t payload1, uint8_t payload2,
+                    uint16_t payload,
                     uint16_t menuI2cPullupValue);
 WarpStatus    configureSensorINA219(uint8_t payload_SETUP1, uint8_t payload_SETUP2, uint8_t payload_CALIB1, uint8_t payload_CALIB2, uint16_t menuI2cPullupValue);
 WarpStatus    readSensorSignalINA219(WarpTypeMask signal,
